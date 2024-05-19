fccace7a4fdb36dac8b3e2e3fd8c0c34475f8fb7
Alpha-4 <aarondadhwal@gmail.com>
2024-05-19 13:27:57 +0530

    added button component and tailwindcss config

--- components/GlassPane.tsx
+++ components/GlassPane.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
+import clsx from "clsx"
 
-const GlassPane = ({children, className}: {children: React.ReactNode, className?: string}) => {
-    return (
-        <div className={'glass rounded-2xl border-solid border-2 border-gray-200' + className ? className : ''}>
-            {children}
-        </div>
-    )
+const GlassPane = ({children, className}: {children: React.ReactNode, className: string}) => {
+    return <div className={clsx('glass rounded-2xl border-solid border-2 border-gray-200', className)}>
+        {children}
+    </div>
 }
 
 export default GlassPane
